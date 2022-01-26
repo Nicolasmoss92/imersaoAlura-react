c137f19e0f5ee5ddb446c516a6ed0e83eb2065a6
Nicolas Moss <56548010+Nicolasmoss92@users.noreply.github.com>
2022-01-25 21:24:05 -0300

    wip: estrutura basica

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75bc23bafffab530"; }
+/******/ 		__webpack_require__.h = function() { return "6488d2d631aa7013"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
