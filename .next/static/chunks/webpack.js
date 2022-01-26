c6b0d2aa16399670b47069337d8089f2620d18b4
Nicolas Moss <56548010+Nicolasmoss92@users.noreply.github.com>
2022-01-25 23:07:11 -0300

    wip: aleraçõs colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6488d2d631aa7013"; }
+/******/ 		__webpack_require__.h = function() { return "e1ecfc6a1711108e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
